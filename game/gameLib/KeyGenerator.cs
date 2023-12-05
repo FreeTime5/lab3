0ac77b794b8102aae7572301b80ec0e45aeb2227
Igor Evtushenko <evtushwork@gmail.com>
2023-12-05 22:01:08 +0300

    version 1.0

---
+++ game/gameLib/KeyGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace gameLib
+{
+    internal class KeyGenerator
+    {
+        internal byte[] key { get; private set; }
+        internal KeyGenerator()
+        {
+            key = RandomNumberGenerator.GetBytes(64);
+        }
+    }
+}
---
+++ game/gameLib/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameLib
+{
+    internal static class Player
+    {
+        public static Turn? MakeTurn(string[] allTurns)
+        {
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out int turn) && turn >= 0 && turn <= allTurns.Length)
+            {
+                if (turn == 0)
+                    return new Turn("exit");
+                return new Turn(allTurns, turn - 1);
+            }
+            if (input == "?")
+                return new Turn("help");
+
+            return null;
+        }
+    }
+}
